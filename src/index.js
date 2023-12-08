f18b29eaddad16f6531ecdc06f1788649c0dc9ba
Utkarsh Sharma <sharmautkarsh107@gmail.com>
2023-12-08 12:24:46 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ]
+});
+
+client.on('ready', (c) => {
+  console.log(`${c.user.username} is ready`)
+})
+
+client.login(process.env.TOKEN);
